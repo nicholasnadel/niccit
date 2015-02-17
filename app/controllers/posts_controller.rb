2dbc3235d9daf5f4657646be5d92627d043dc7bf
nicholasnadel <nicholasnadel@gmail.com>
2015-02-17 15:16:49 -0800

    Added pagination, fixed markdown in comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,7 @@ class PostsController < ApplicationController
       @post = Post.find(params[:id])
       authorize @post
     end
-
+    
     def create
       @topic = Topic.find(params[:topic_id])
       @post = current_user.posts.build(post_params)
@@ -30,13 +30,13 @@ class PostsController < ApplicationController
         render :new
       end
     end
-
+    
     def update
       @topic = Topic.find(params[:topic_id])
       @post = Post.find(params[:id])
       authorize @post
 
-      @post = current_user.posts.build(post_params)
+      if @post.update_attributes(post_params)
         flash[:notice] = "Post was updated."
         redirect_to [@topic, @post]
       else
@@ -48,7 +48,6 @@ class PostsController < ApplicationController
     private
 
     def post_params
-      params.require(:post).permit(:title, :body)
+      params.require(:post).permit(:title, :body, :image)
     end
-    
   end
