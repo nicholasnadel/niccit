da7d8d691d08f08deccf86bb628cc38fbe34e68d
nicholasnadel <nicholasnadel@gmail.com>
2015-02-25 21:29:01 -0800

    added commenting ability

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,19 @@
 class User < ActiveRecord::Base
-  has_many :posts
-   mount_uploader :avatar, AvatarUploader
-   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable#, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-   def admin?
-     role == 'admin'
-   end
-   
-   def moderator?
-     role == 'moderator'
-   end  
+  has_many :posts
+  has_many :comments
+  
+  mount_uploader :avatar, AvatarUploader
+
+  def admin?
+    role == 'admin'
+  end
+
+  def moderator?
+    role == 'moderator'
+  end
 end
