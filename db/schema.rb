f1f3e7039c1dceb57fed8fdeb0eaabfe2d62fcdf
nicholasnadel <nicholasnadel@gmail.com>
2015-02-27 12:53:04 -0800

    added private topics

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227192812) do
+ActiveRecord::Schema.define(version: 20150227201646) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -57,12 +57,12 @@ ActiveRecord::Schema.define(version: 20150227192812) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "email",                  default: "",   null: false
-    t.string   "encrypted_password",     default: "",   null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,    null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -75,7 +75,6 @@ ActiveRecord::Schema.define(version: 20150227192812) do
     t.datetime "updated_at"
     t.string   "role"
     t.string   "avatar"
-    t.boolean  "email_favorites",        default: true
     t.boolean  "email_permissions"
   end
 
